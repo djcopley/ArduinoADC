6d6d7d3d6e0f599fbaf4b8bfc5a961fa657969e4
Daniel Copley <djcopley@users.noreply.github.com>
2019-02-26 13:48:54 -0700

    Added Seven Segment class

---
+++ seven_segment.h
@@ -0,0 +1,14 @@
+//
+// Created by Daniel Copley on 2019-02-26.
+//
+
+#ifndef ARDUINOADC_INO_SEVEN_SEGMENT_H
+#define ARDUINOADC_INO_SEVEN_SEGMENT_H
+
+
+class seven_segment {
+
+};
+
+
+#endif //ARDUINOADC_INO_SEVEN_SEGMENT_H
