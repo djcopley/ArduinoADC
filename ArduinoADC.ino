b6cac147c2e59abaa4b54506544954b747e02732
djcopley <djrcopley@icloud.com>
2019-02-26 13:48:54 -0700

    Added Seven Segment class

---
+++ ArduinoADC.ino
@@ -0,0 +1,19 @@
+/**
+ * Group 3 - Arduino Analog to Digital Converter
+ *
+ * Daniel Copley, Alenn Wright, Corey Cline, Jimmy Banh
+ */
+
+// MACROS
+
+// PIN DECLARATIONS
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
---
+++ seven_segment.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Daniel Copley on 2019-02-26.
+//
+
+#include "seven_segment.h"
