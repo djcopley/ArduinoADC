0b8a73a35838ce0b98e4ac40712af0b34f44017b
djcopley <djrcopley@icloud.com>
2019-03-09 15:46:19 -0700

    Added ADC class

---
+++ lib/ADC.cpp
@@ -0,0 +1,5 @@
+//
+// Created by djcopley on 3/9/19.
+//
+
+#include "ADC.h"
---
+++ lib/ADC.h
@@ -0,0 +1,14 @@
+//
+// Created by djcopley on 3/9/19.
+//
+
+#ifndef ARDUINOADC_ADC_H
+#define ARDUINOADC_ADC_H
+
+
+class ADC {
+
+};
+
+
+#endif //ARDUINOADC_ADC_H
